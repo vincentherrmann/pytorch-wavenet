4492ed1f21ebcc496fbc6a75380cb3a82546a8a8
vincentherrmann <vincent.herrmann@web.de>
2017-05-16 21:24:18 +0200

    Minor updates

--- wavenet_training.py
+++ wavenet_training.py
@@ -15,36 +15,44 @@ def print_last_loss(losses):
     print("loss: ", losses[-1])
 
 
+def print_last_test_result(test_losses):
+    print("test loss: ", test_losses[-1])
+
+
 class WaveNetOptimizer:
     def __init__(self,
                  model,
-                 mini_batch_size,
+                 data,
+                 test_segments=0,
+                 examples_per_test_segment=8,
                  optimizer=optim.Adam,
-                 learning_rate=0.001,
                  report_callback=print_last_loss,
-                 report_length=20,
-                 test_interval=40):
+                 report_interval=8,
+                 test_report_callback=print_last_test_result,
+                 test_interval=64,
+                 snapshot_interval=256,
+                 snapshot_file=None):
 
         self.model = model
-        self.mini_batch_size = mini_batch_size
-        self.optimizer = optimizer(params=self.model.parameters(),
-                                   lr=learning_rate)
+        self.data = data
+        self.data.epoch_finished_callback = self.new_epoch
+        self.optimizer = optimizer(params=self.model.parameters())
+
+        if test_segments > 0:
+            self.data.create_test_set(segments=test_segments,
+                                      examples_per_segment=examples_per_test_segment)
+
         self.report_callback = report_callback
-        self.report_length = report_length
+        self.report_interval = report_interval
+        self.test_report_callback = test_report_callback
         self.test_interval = test_interval
+        self.snapshot_interval = snapshot_interval
+        self.snapshot_file = snapshot_file
+
         self.current_epoch = 0
         self.epochs = 1
         self.segments_per_chunk = 16
         self.examples_per_segment = 32
-        self.data = None
-
-    # def new_training_order(self, index_count):
-    #     l = self.model.output_length
-    #     samples_count = index_count // l
-    #     order = torch.randperm(samples_count - 1)
-    #     offset = randint(0, l - 1)
-    #     indices = [i * l + offset for i in order]
-    #     return indices
 
     def new_epoch(self):
         self.current_epoch += 1
@@ -62,9 +70,9 @@ class WaveNetOptimizer:
         i = 0
 
         while i < self.data.test_index_count:
-            inputs = self.data.test_inputs[i:(i+test_m),:,:]
+            inputs = self.data.test_inputs[i:(i+test_m), :, :]
             inputs = Variable(inputs, volatile=True)
-            targets = self.data.test_targets[i:(i+test_m),:]
+            targets = self.data.test_targets[i:(i+test_m), :]
             targets = targets.view(targets.size(0) * targets.size(1))
             targets = Variable(targets, volatile=True)
             output = self.model(inputs)
@@ -74,19 +82,19 @@ class WaveNetOptimizer:
 
         avg_loss = avg_loss * test_m / self.data.test_index_count
         print("test loss: ", avg_loss)
+        self.model.train()
 
     def train(self,
-              data,
-              epochs=10,
+              learning_rate=0.001,
+              minibatch_size=8,
+              epochs=100,
               segments_per_chunk=16,
-              examples_per_segment=32,
-              snapshot_interval=1800,
-              snapshot_file=None,
-              test_segments=16,
-              examples_per_test_segment=4):
+              examples_per_segment=32):
 
-        # create test set
-        data.create_test_set(segments=test_segments, examples_per_segment=examples_per_test_segment)
+        self.optimizer.lr = learning_rate
+        self.epochs = epochs
+        self.segments_per_chunk = segments_per_chunk
+        self.examples_per_segment = examples_per_segment
 
         self.model.train()  # set to train mode
         i = 0  # step index
@@ -94,13 +102,6 @@ class WaveNetOptimizer:
         losses = []
         previous_loss = 1000
         self.current_epoch = -1
-        self.epochs = epochs
-        self.segments_per_chunk = segments_per_chunk
-        self.examples_per_segment = examples_per_segment
-        self.data = data
-        self.data.epoch_finished_callback = self.new_epoch
-
-        tic = time.time()
 
         self.new_epoch()
         self.data.load_new_chunk()
@@ -111,7 +112,7 @@ class WaveNetOptimizer:
             self.optimizer.zero_grad()
 
             # get data
-            inputs, targets = self.data.get_minibatch(self.mini_batch_size)
+            inputs, targets = self.data.get_minibatch(minibatch_size)
             targets = targets.view(targets.size(0) * targets.size(1))
             inputs = Variable(inputs)
             targets = Variable(targets)
@@ -131,25 +132,24 @@ class WaveNetOptimizer:
             i += 1
 
             # train feedback
-            if i % self.report_length == 0:
-                avg_loss /= self.report_length
+            if i % self.report_interval == 0:
+                avg_loss /= self.report_interval
                 previous_loss = avg_loss
                 losses.append(avg_loss)
                 if self.report_callback != None:
                     self.report_callback(losses)
                 avg_loss = 0
 
+            # run on test set
             if i % self.test_interval == 0:
                 self.test_model()
 
             # snapshot
-            toc = time.time()
-            if toc - tic >= snapshot_interval:
-                if snapshot_file != None:
-                    torch.save(self.model.state_dict(), snapshot_file)
+            if i % self.snapshot_interval == 0:
+                if self.snapshot_file != None:
+                    torch.save(self.model.state_dict(), self.snapshot_file)
                     date = str(datetime.now())
-                    print(date, ": snapshot saved to ", snapshot_file)
-                tic = toc
+                    print(date, ": snapshot saved to ", self.snapshot_file)
 
 
 class AudioFileLoader:
@@ -158,7 +158,6 @@ class AudioFileLoader:
                  classes,
                  receptive_field,
                  target_length,
-                 max_load_duration=3600,
                  dtype=torch.FloatTensor,
                  ltype=torch.LongTensor,
                  sampling_rate=11025,
@@ -208,11 +207,6 @@ class AudioFileLoader:
             self.data_duration += d
             self.start_positions.append(self.data_duration)
         print("total duration of training data: ", self.data_duration, " s")
-
-        if self.data_duration < max_load_duration:
-            self.max_load_duration = self.data_duration
-        else:
-            self.max_load_duration = max_load_duration
 
             # self.start_new_epoch()
 
@@ -330,9 +324,6 @@ class AudioFileLoader:
 
     def use_new_chunk(self):
         print("use loaded chunk with ", len(self.loaded_data), "segments")
-        # if len(self.loaded_data) == 0:
-        #     if self.epoch_finished_callback != None:
-        #         self.epoch_finished_callback()
 
         # wait for loading to finish
         if self.load_thread != None:
@@ -412,97 +403,3 @@ class AudioFileLoader:
             new_data = np.append(new_data, additional_data)
 
         return new_data
-
-
-
-    # def get_wavenet_minibatch(self, indices, receptive_field, target_length):
-    #     n = len(indices)
-    #     input = self.dtype(n, 1, receptive_field).zero_()
-    #     target = self.ltype(n, target_length).zero_()
-    #
-    #     for i in range(n):
-    #         index = indices[i] + 1
-    #
-    #         sample_length = min(index, receptive_field)
-    #         input[i, :, -sample_length:] = self.inputs[index - sample_length:index]
-    #
-    #         sample_length = min(index, target_length)
-    #         target[i, -sample_length:] = self.targets[index - sample_length:index]
-    #
-    #     return input, target
-    #
-    #
-    #
-    # def start_new_epoch(self, shuffle_files=True):
-    #     '''
-    #     Start a new epoch. The order of the files can be shuffled.
-    #     '''
-    #
-    #     print("new epoch")
-    #     if shuffle_files:
-    #         shuffle(self.paths)
-    #     self.current_file = 0
-    #     self.current_offset = 0
-    #
-    #     self.load_new_chunk(self.max_load_duration)
-    #     return self.use_new_chunk()
-    #
-    # def use_new_chunk(self):
-    #     '''
-    #     Move the previously loaded data into a pytorch (cuda-)tensor and loads the next chunk in a background thread
-    #     '''
-    #     self.inputs = self.dtype(self.loaded_data[0])
-    #     self.targets = self.ltype(self.loaded_data[1])
-    #
-    #     while self.load_thread.is_alive():
-    #         # wait...
-    #         i = 0
-    #
-    #     # If there are no files left, return 0
-    #     if self.current_file >= len(self.paths):
-    #         # return self.start_new_epoch()
-    #         return 0
-    #
-    #     self.load_thread = threading.Thread(target=self.load_new_chunk, args=[self.max_load_duration])
-    #     self.load_thread.start()
-    #     return self.inputs.size(0)
-    #
-    #
-    #
-    # def load_new_chunk(self, duration):
-    #     '''
-    #     Load a new chunk of data with the specified duration (in seconds) from multiple disc files into memory.
-    #     For better performance, this can be called in a background thread.
-    #     '''
-    #     print("load new chunk")
-    #     loaded_duration = 0
-    #     data = np.array(0)
-    #
-    #     # loop though files until the specified duration is loaded
-    #     while loaded_duration < duration:
-    #         file = self.paths[self.current_file]
-    #         remaining_duration = duration - loaded_duration
-    #         print("data from ", file)
-    #         # print("file number: ", self.current_file)
-    #         # print("offset: ", self.current_offset)
-    #         new_data, sr = librosa.load(self.paths[self.current_file],
-    #                                     sr=self.sampling_rate,
-    #                                     mono=True,
-    #                                     offset=self.current_offset,
-    #                                     duration=remaining_duration)
-    #         new_loaded_duration = len(new_data) / self.sampling_rate
-    #         data = np.append(data, new_data)
-    #         loaded_duration += new_loaded_duration
-    #
-    #         if new_loaded_duration < remaining_duration:  # if this file was loaded completely
-    #             # move to next file
-    #             self.current_file += 1
-    #             self.current_offset = 0
-    #             # break if not enough data is available
-    #             if self.current_file >= len(self.paths):
-    #                 break
-    #         else:
-    #             self.current_offset += new_loaded_duration
-    #
-    #     data = self.quantize_data(data)
-    #     self.loaded_data = data
