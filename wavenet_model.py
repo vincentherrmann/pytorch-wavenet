7fee255c145856e958c5f69ffe2f78aa24f72182
vincentherrmann <vincent.herrmann@web.de>
2017-12-27 11:22:05 +0100

    Add additional end convolution, change back to sigmoid/tanh nonlinearity.

--- wavenet_model.py
+++ wavenet_model.py
@@ -30,7 +30,8 @@ class WaveNetModel(nn.Module):
                  blocks=4,
                  dilation_channels=32,
                  residual_channels=32,
-                 skip_channels=64,
+                 skip_channels=256,
+                 end_channels=256,
                  classes=256,
                  output_length=32,
                  kernel_size=2,
@@ -52,13 +53,13 @@ class WaveNetModel(nn.Module):
         receptive_field = 1
         init_dilation = 1
 
-        print("relu network")
+        #print("relu network")
 
         self.dilations = []
         self.dilated_queues = []
-        self.main_convs = nn.ModuleList()
-        # self.filter_convs = nn.ModuleList()
-        # self.gate_convs = nn.ModuleList()
+        # self.main_convs = nn.ModuleList()
+        self.filter_convs = nn.ModuleList()
+        self.gate_convs = nn.ModuleList()
         self.residual_convs = nn.ModuleList()
         self.skip_convs = nn.ModuleList()
 
@@ -81,21 +82,21 @@ class WaveNetModel(nn.Module):
                                                         dilation=new_dilation,
                                                         dtype=dtype))
 
-                self.main_convs.append(nn.Conv1d(in_channels=residual_channels,
+                # self.main_convs.append(nn.Conv1d(in_channels=residual_channels,
+                #                                    out_channels=dilation_channels,
+                #                                    kernel_size=kernel_size,
+                #                                    bias=bias))
+
+                # dilated convolutions
+                self.filter_convs.append(nn.Conv1d(in_channels=residual_channels,
                                                    out_channels=dilation_channels,
                                                    kernel_size=kernel_size,
                                                    bias=bias))
 
-                # dilated convolutions
-                # self.filter_convs.append(nn.Conv1d(in_channels=residual_channels,
-                #                                    out_channels=dilation_channels,
-                #                                    kernel_size=kernel_size,
-                #                                    bias=bias))
-                #
-                # self.gate_convs.append(nn.Conv1d(in_channels=residual_channels,
-                #                                  out_channels=dilation_channels,
-                #                                  kernel_size=kernel_size,
-                #                                  bias=bias))
+                self.gate_convs.append(nn.Conv1d(in_channels=residual_channels,
+                                                 out_channels=dilation_channels,
+                                                 kernel_size=kernel_size,
+                                                 bias=bias))
 
                 # 1x1 convolution for residual connection
                 self.residual_convs.append(nn.Conv1d(in_channels=dilation_channels,
@@ -115,10 +116,15 @@ class WaveNetModel(nn.Module):
                 init_dilation = new_dilation
                 new_dilation *= 2
 
-        self.end_conv = nn.Conv1d(in_channels=skip_channels,
-                                  out_channels=classes,
+        self.end_conv_1 = nn.Conv1d(in_channels=skip_channels,
+                                  out_channels=end_channels,
                                   kernel_size=1,
                                   bias=True)
+
+        self.end_conv_2 = nn.Conv1d(in_channels=end_channels,
+                                    out_channels=classes,
+                                    kernel_size=1,
+                                    bias=True)
 
         # self.output_length = 2 ** (layers - 1)
         self.output_length = output_length
@@ -146,13 +152,13 @@ class WaveNetModel(nn.Module):
             residual = dilation_func(x, dilation, init_dilation, i)
 
             # dilated convolution
-            # filter = self.filter_convs[i](residual)
-            # filter = F.tanh(filter)
-            # gate = self.gate_convs[i](residual)
-            # gate = F.sigmoid(gate)
-            # x = filter * gate
-            preativation = self.main_convs[i](residual)
-            x = F.relu(preativation)
+            filter = self.filter_convs[i](residual)
+            filter = F.tanh(filter)
+            gate = self.gate_convs[i](residual)
+            gate = F.sigmoid(gate)
+            x = filter * gate
+            # preativation = self.main_convs[i](residual)
+            # x = F.relu(preativation)
 
             # parametrized skip connection
             s = x
@@ -170,7 +176,8 @@ class WaveNetModel(nn.Module):
             x = x + residual[:, :, (self.kernel_size - 1):]
 
         x = F.relu(skip)
-        x = self.end_conv(x)
+        x = F.relu(self.end_conv_1(x))
+        x = self.end_conv_2(x)
 
         return x
 
@@ -219,7 +226,7 @@ class WaveNetModel(nn.Module):
                              dilation_func=self.wavenet_dilate)[-1, :].squeeze()
 
             if temperature > 0:
-                x = x / temperature
+                x /= temperature
                 prob = F.softmax(x, dim=0)
                 prob = prob.cpu()
                 np_prob = prob.data.numpy()
@@ -244,6 +251,7 @@ class WaveNetModel(nn.Module):
                       num_samples,
                       first_samples=None,
                       temperature=1.,
+                      regularize=0.,
                       progress_callback=None,
                       progress_interval=100):
         self.eval()
@@ -280,14 +288,19 @@ class WaveNetModel(nn.Module):
 
         # generate new samples
         generated = np.array([])
+        regularizer = torch.pow(Variable(torch.arange(self.classes)) - self.classes / 2., 2)
+        regularizer = regularizer.squeeze() * regularize
         tic = time.time()
         for i in range(num_samples):
             x = self.wavenet(input,
-                             dilation_func=self.queue_dilate)
+                             dilation_func=self.queue_dilate).squeeze()
+            #regularizer = torch.abs(Variable(torch.arange(self.classes)) - torch.max(input, 1)[1].type(self.dtype))
+
+            x = x - regularizer
 
             if temperature > 0:
                 # sample from softmax distribution
-                x = x.squeeze() / temperature
+                x /= temperature
                 prob = F.softmax(x, dim=0)
                 prob = prob.cpu()
                 np_prob = prob.data.numpy()
@@ -295,7 +308,6 @@ class WaveNetModel(nn.Module):
                 x = np.array([x])
             else:
                 # convert to sample value
-                x = x.squeeze()
                 x = torch.max(x, 0)[1][0]
                 x = x.cpu()
                 x = x.data.numpy()
@@ -322,12 +334,18 @@ class WaveNetModel(nn.Module):
 
         self.train()
         mu_gen = mu_law_expansion(generated, self.classes)
-        return  mu_gen
+        return mu_gen
 
     def parameter_count(self):
         par = list(self.parameters())
         s = sum([np.prod(list(d.size())) for d in par])
         return s
+
+    def cpu(self, type=torch.FloatTensor):
+        self.dtype = type
+        for q in self.dilated_queues:
+            q.dtype = self.dtype
+        super().cpu()
 
 
 def load_latest_model_from(location, use_cuda=True):
@@ -338,6 +356,12 @@ def load_latest_model_from(location, use_cuda=True):
     if use_cuda:
         model = torch.load(newest_file)
     else:
-        model = torch.load(newest_file, map_location=lambda storage, loc: storage)
+        model = load_to_cpu(newest_file)
 
+    return model
+
+
+def load_to_cpu(path):
+    model = torch.load(path, map_location=lambda storage, loc: storage)
+    model.cpu()
     return model
